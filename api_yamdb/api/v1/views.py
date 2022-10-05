e872e1cde5292c8ba0435b661b44547290860f7d
Alexey Shapiro <academ@gmail.com>
2022-10-06 00:13:52 +0300

    добавил токены, сделал пару вью-функций как скелет, который надо дорабатывать, опять обновил модель User, код уже проходит 2 теста (из 53) :)

---
+++ api_yamdb/api/v1/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import get_object_or_404
+from .serializers import get_tokens_for_user, UserRegistrationSerializer
+from reviews.models import User
+from rest_framework import filters, viewsets, mixins, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+class UserRegistrationViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    serializer_class = UserRegistrationSerializer
+    queryset = User.objects.all()
+    print()
+
+
+@api_view(['POST'])
+def get_token(request):
+    for arg in ('username', 'confirmation_code'):
+        if not request.data.get(arg):
+            return Response(
+                {arg: ["This field is required."]},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    user = get_object_or_404(User, username=request.data['username'])
+    if request.data['confirmation_code'] == user.get_hash():
+        user.email_is_verified = True
+        user.save()
+        return Response(get_tokens_for_user(user), status=status.HTTP_200_OK)
+    return Response(
+        f'Confirmation code {request.data["confirmation_code"]} is  incorrect!'
+        ' Be sure to request it again',
+        status=status.HTTP_400_BAD_REQUEST
+    )
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,6 +2,7 @@ import logging
 import os
 import sys
 
+from datetime import timedelta
 from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -49,7 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reviews',
-    'api'
+    'api',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,41 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 AUTH_USER_MODEL = 'reviews.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': TOKEN_SECRET,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
