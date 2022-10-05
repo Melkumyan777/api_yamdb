5ea7b1229d4caafaeffb7bb8e2410550ccafaea9
Alexey Shapiro <academ@gmail.com>
2022-10-05 11:17:22 +0300

    user model skeleton added

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+USER_ROLES = (
+    ('user', 'User'),
+    ('moder', 'Moderator'),
+    ('admin', 'Admin')
+)
+
+
+class User(AbstractUser):
+    role = models.CharField(choices=USER_ROLES, default='user', max_length=5)
+    is_superuser = models.BooleanField(default=False)
